0767fec54d339b3779a9b555d15dea03b9189bbd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-21 16:19:17 +0200

    rename function

---
+++ Public/Get-JamfDevice.ps1
@@ -0,0 +1,26 @@
+﻿function Get-JamfDevice {
+    [CmdletBinding()]
+    param(
+        [string] $Id,
+        [switch] $Native
+    )
+    $Organization = $Script:JamfTokenInformation.Organization
+    $Url = "https://$Organization.jamfcloud.com/api/v1/computers-inventory"
+    if ($id) {
+        $Url = "$Url/$id"
+    }
+    $Data = Invoke-JamfQuery -Url $Url
+    if ($Native) {
+        if ($Id) {
+            $Data
+        } else {
+            $Data.results
+        }
+    } else {
+        if ($Id) {
+            Convert-JamfDevice -Devices $Data
+        } else {
+            Convert-JamfDevice -Devices $Data.results
+        }
+    }
+}
