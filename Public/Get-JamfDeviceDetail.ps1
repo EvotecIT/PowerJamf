42b310f3113e2d6eb9c6df24f3637efce8f5937b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-21 16:19:28 +0200

    add function with detailed devices

---
+++ Public/Get-JamfDeviceDetail.ps1
@@ -0,0 +1,13 @@
+﻿function Get-JamfDeviceDetail {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Id
+    )
+    $Organization = $Script:JamfTokenInformation.Organization
+    $Url = "https://$Organization.jamfcloud.com/api/v1/computers-inventory-detail"
+    if ($id) {
+        $Url = "$Url/$id"
+    }
+    $Data = Invoke-JamfQuery -Url $Url
+    $Data
+}
