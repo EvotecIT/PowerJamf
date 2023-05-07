d9e47d8c7134140c46afd42752fa3ed14167d9aa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-07 15:27:42 +0200

    Update module builder

--- Build/Build-ModuleSimplified.ps1
+++ Build/Build-ModuleSimplified.ps1
@@ -74,5 +74,5 @@ New-PrepareModule -ModuleName 'PowerJamf' {
 
     # global options for publishing to github/psgallery
     #New-ConfigurationPublish -Type PowerShellGallery -FilePath 'C:\Support\Important\PowerShellGalleryAPI.txt' -Enabled:$true
-    New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
+    #New-ConfigurationPublish -Type GitHub -FilePath 'C:\Support\Important\GitHubAPI.txt' -UserName 'EvotecIT' -Enabled:$true
 }
