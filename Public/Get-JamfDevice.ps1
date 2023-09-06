16820e87a17a6f676fd6b1f56bc2fe43b08191ab
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-06 08:33:37 +0200

    Fix critical bug returning only 100 computers

--- Public/Get-JamfDevice.ps1
+++ Public/Get-JamfDevice.ps1
@@ -2,13 +2,34 @@
     [CmdletBinding()]
     param(
         [string] $Id,
-        [switch] $Native
+        [switch] $Native,
+        [int] $Page,
+        [int] $PageSize = 1000000
     )
     $Organization = $Script:JamfTokenInformation.Organization
-    $Url = "https://$Organization.jamfcloud.com/api/v1/computers-inventory"
+
+    $QueryParameter = [ordered] @{
+        'page-size' = if ($PSBoundParameters.ContainsKey('Id')) { $null } else { $PageSize }
+        'page'      = if ($PSBoundParameters.ContainsKey('Page')) { $Page } else { $null }
+    }
+    # lets remove empty values to remove whatever user hasn't requested
+    Remove-EmptyValue -Hashtable $QueryParameter
+
+    if (-not $Organization) {
+        Write-Warning -Message 'Get-JamfDevice - Organization not set. Use Connect-Jamf to set it.'
+        return
+    }
+
+    # Lets build our url
+    $BaseUri = "https://$Organization.jamfcloud.com/api/v1/computers-inventory"
+    $Url = Join-UriQuery -BaseUri $BaseUri -QueryParameter $QueryParameter
+
     if ($id) {
         $Url = "$Url/$id"
     }
+
+    Write-Verbose -Message "Get-JamfDevice - Using query: $Url"
+
     $Data = Invoke-JamfQuery -Url $Url
     if ($Native) {
         if ($Id) {
